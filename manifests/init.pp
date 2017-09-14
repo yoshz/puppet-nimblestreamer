ac3f205a8c36fe4727e6ccf9fc9405e1c5da9225
Yosh de Vos <yosh@elzorro.nl>
2017-09-14 16:50:52 +0200

    Added support for additional access control and certificate configuration options

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,14 +23,24 @@
 # Yosh de Vos <yosh@elzorro.nl>
 #
 class nimblestreamer (
-  Integer $port                = 8081,
-  String  $logging             = 'info',
-  Integer $worker_threads      = 1,
-  Integer $max_cache_size      = 64,
-  Integer $max_disk_cache_size = 4096,
-  String  $cache_path          = '/var/cache/nimble',
-  String  $panel_uuid          = '',
-  String  $panel_password      = '',
+  Integer           $port                = 8081,
+  String            $logging             = 'info',
+  Integer           $worker_threads      = 1,
+  Integer           $max_cache_size      = 64,
+  Integer           $max_disk_cache_size = 4096,
+  String            $cache_path          = '/var/cache/nimble',
+  Optional[String]  $panel_uuid          = undef,
+  Optional[String]  $panel_password      = undef,
+  Optional[Integer] $ssl_port            = undef,
+  Optional[String]  $ssl_certificate     = undef,
+  Optional[String]  $ssl_certificate_key = undef,
+  Optional[String]  $ssl_certificate_key_pass = undef,
+  Optional[String]  $crossdomain_xml_allow_access_from_domain = undef,
+  Optional[String]  $crossdomain_xml     = undef,
+  Optional[String]  $access_control_allow_origin = undef,
+  Optional[String]  $access_control_allow_credentials = undef,
+  Optional[String]  $access_control_expose_headers = undef,
+  Optional[String]  $access_control_allow_headers = undef,
 ) {
   include nimblestreamer::install
   file { '/etc/nimble/nimble.conf':
